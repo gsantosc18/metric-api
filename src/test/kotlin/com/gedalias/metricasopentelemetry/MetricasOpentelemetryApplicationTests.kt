c214bf22ba224d3e2150fa75c5aaf956121337a2
Gedalias dos Santos Caldas <gsantos.c18@gmail.com>
2023-08-06 11:50:27 -0300

    Inicialização do repositório

---
+++ src/test/kotlin/com/gedalias/metricasopentelemetry/MetricasOpentelemetryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gedalias.metricasopentelemetry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MetricasOpentelemetryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
