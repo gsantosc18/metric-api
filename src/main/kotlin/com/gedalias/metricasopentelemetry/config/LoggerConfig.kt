5762360a2fcb6bb96666019739d175ade34ba63e
Gedalias dos Santos Caldas <gsantos.c18@gmail.com>
2023-09-03 10:57:21 -0300

    feat: Logs de ações do usuário

---
+++ src/main/kotlin/com/gedalias/metricasopentelemetry/config/LoggerConfig.kt
@@ -0,0 +1,25 @@
+package com.gedalias.metricasopentelemetry.config
+
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.LoggerContext
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
+
+//@Configuration
+class LoggerConfig {
+    @Bean
+    fun logger(): Logger {
+        val context = LoggerFactory.getILoggerFactory() as LoggerContext
+        return context.getLogger(Logger.ROOT_LOGGER_NAME)
+            .apply {
+                addAppender(buildLogAppender(context))
+           }
+    }
+
+    private fun buildLogAppender(context: LoggerContext) =
+        LogAppender()
+            .apply {
+                this.context = context
+                start()
+            }
+}
