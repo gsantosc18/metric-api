1ce30d6372a2d8da4ea60b8ccb2602735cdf3839
Gedalias dos Santos Caldas <gsantos.c18@gmail.com>
2023-11-03 15:32:02 -0300

    feat: Add logback configuration

--- src/main/kotlin/com/gedalias/metricasopentelemetry/MetricasOpentelemetryApplication.kt
+++ src/main/kotlin/com/gedalias/metricasopentelemetry/MetricasOpentelemetryApplication.kt
@@ -1,5 +1,6 @@
 package com.gedalias.metricasopentelemetry
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
